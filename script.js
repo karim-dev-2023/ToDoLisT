59a6c2968882da51f7626c01fee07b7e5b049d77
karim-dev-2023 <131853616+karim-dev-2023@users.noreply.github.com>
2025-02-17 16:00:30 +0100

    Code Structured

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
+let arrayToDolist = [];
 const addBtn = document.getElementById("addBtn");
 const doToSection = document.getElementById("doToSection");
 
 addBtn.addEventListener("click", () => {
-  // Ceci permet de saisir un texte dans un prompt
   const nameTask = window.prompt("Please enter the name of task :");
-  if (nameTask != null && nameTask != "") {
-    doToSection.innerHTML += `
-    <article class="p-2 bg-warning-subtle rounded-pill mb-2 d-flex justify-content-between">
-        <div>
-        ${nameTask}
-        </div>
-        <button class="btn btn-sm btn-danger rounded-circle" onclick='alert("OK")'">
-        X
-        </button>
-    </article>`;
 
-    localStorage.setItem("doToListTask", doToSection.innerHTML);
-  } else {
-    window.alert("You didn't get anything");
-  }
+  addTasks(nameTask);
+  showTasks();
 
-  //   console.log(localStorage.getItem("doToListTask"));
+  // // Ceci permet de saisir un texte dans un prompt
+  // if (nameTask != null && nameTask != "") {
+  //   doToSection.innerHTML += `
+  //   <article class="p-2 bg-warning-subtle rounded-pill mb-2 d-flex justify-content-between">
+  //       <div>
+  //       ${nameTask}
+  //       </div>
+  //       <button class="btn btn-sm btn-danger rounded-circle" onclick='alert("OK")'">
+  //       X
+  //       </button>
+  //   </article>`;
+
+  //   localStorage.setItem("doToListTask", doToSection.innerHTML);
+  // } else {
+  //   window.alert("You didn't get anything");
+  // }
+
+  // console.log(localStorage.getItem("doToListTask"));
 });
 
 const localStorageToDoList = localStorage.getItem("doToListTask");
-
-if (localStorageToDoList) {
-  doToSection.innerHTML = localStorageToDoList;
-}
+console.log(localStorageToDoList);
 
 // Elle permet de supprimer tous ce qui se toruve sur le localstorage
 // localStorage.clear();
+
+function addTasks(nameTask) {
+  doToSection.innerHTML = "";
+  if (nameTask) {
+    arrayToDolist.push(nameTask);
+  } else {
+    window.alert("You didn't get anything");
+  }
+}
+
+function showTasks() {
+  // Je créer l'élément article en utilisant le createElement
+  var article = document.createElement("article");
+
+  for (let taskName of arrayToDolist) {
+    let taskElement = document.createElement("p"); // Crée un élément <p>
+    taskElement.textContent = taskName; // Assigne le texte
+    article.appendChild(taskElement); // Ajoute l'élément <p> à l'article
+  }
+
+  doToSection.appendChild(article); // Ajoute l'article à la section
+  localStorage.setItem("doToListTask", arrayToDolist);
+}
+
